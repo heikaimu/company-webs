46f7b12ecd8d115233e2cfc7100a9bda821d4a4e
Liu YaoWen <576079353@qq.com>
2020-09-04 15:16:46 +0800

    1

--- lawyer-person/js/main.js
+++ lawyer-person/js/main.js
@@ -4,12 +4,43 @@
  * @Autor: Yaowen Liu
  * @Date: 2020-05-02 17:17:19
  * @LastEditors: Yaowen Liu
- * @LastEditTime: 2020-09-03 13:25:49
+ * @LastEditTime: 2020-09-04 13:17:47
  */
 $(function () {
 
+  // 导航
   var nav = $(this).find(".web-nav");
   nav.lavaLamp({ fx: "swing", speed: 500 });
+
+  // 轮播图
+  new Swiper("#banner", {
+    loop: true,
+    speed: 500,
+    effect: 'fade',
+    autoplay: {
+      delay: 5000
+    },
+    disableOnInteraction: false,
+    pagination: {
+      el: $("#banner .swiper-pagination"),
+      clickable: true
+    },
+    on: {
+      init: function () {
+        swiperAnimateCache(this); //隐藏动画元素 
+        swiperAnimate(this); //初始化完成开始动画
+      },
+      slideChangeTransitionEnd: function () {
+        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        //this.slides.eq(this.activeIndex).find('.ani').removeClass('ani'); 动画只展现一次，去除ani类名
+      }
+    }
+  });
+
+  // 内部导航
+  new Swiper("#inline_nav", {
+    slidesPerView: 9,
+  });
 
 })
 
