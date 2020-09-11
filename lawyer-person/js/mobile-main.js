9be2bd81614fb9fab392572432d0135c4d832d38
Liu YaoWen <576079353@qq.com>
2020-09-11 15:52:46 +0800

    1

---
+++ lawyer-person/js/mobile-main.js
@@ -0,0 +1,58 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: Yaowen Liu
+ * @Date: 2020-09-09 18:10:03
+ * @LastEditors: Yaowen Liu
+ * @LastEditTime: 2020-09-10 16:40:29
+ */
+$(function () {
+
+  // 导航
+  var header = $("#header");
+  var openMenu = header.find(".open-menu");
+  var closeMenu = header.find(".close-menu");
+  var navContent = header.find(".web-nav .content");
+  var navBlank = header.find(".web-nav .blank");
+
+  // 打开
+  openMenu.click(function () {
+    navBlank.fadeIn();
+    navContent.addClass("active");
+  })
+
+  // 关闭
+  navBlank.click(function () {
+    navBlank.fadeOut();
+    navContent.removeClass("active");
+  })
+  closeMenu.click(function () {
+    navBlank.trigger("click");
+  })
+
+  // 轮播图
+  new Swiper("#banner", {
+    loop: true,
+    speed: 500,
+    effect: 'fade',
+    autoplay: {
+      delay: 5000
+    },
+    disableOnInteraction: false,
+    pagination: {
+      el: $("#banner .swiper-pagination"),
+      clickable: true
+    },
+    on: {
+      init: function () {
+        swiperAnimateCache(this); //隐藏动画元素 
+        swiperAnimate(this); //初始化完成开始动画
+      },
+      slideChangeTransitionEnd: function () {
+        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        //this.slides.eq(this.activeIndex).find('.ani').removeClass('ani'); 动画只展现一次，去除ani类名
+      }
+    }
+  });
+
+})
